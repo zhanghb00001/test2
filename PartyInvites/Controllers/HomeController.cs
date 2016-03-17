7d47192190435b18dc7a8eb24399d34bcafdb68a
zhanghb00001 <zhanghb00001@yahoo.com>
2016-03-16 22:37:25 -0700

    A

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            ViewBag.Greeting = DateTime.Now.ToLongTimeString();
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+            // TODO: Email response to the party organizer
+           
+        }
+
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="name required")]
+        public string Name { get; set; }
+
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
